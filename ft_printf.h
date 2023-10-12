f8c6c2b95ed7d030b5ef4b5f312ba534cf44eb3f
Err-G <eriklustosa@gmail.com>
2023-10-12 19:38:03 +0100

    changes

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ecarvalh <ecarvalh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 15:12:09 by ecarvalh          #+#    #+#             */
-/*   Updated: 2023/10/11 15:58:34 by ecarvalh         ###   ########.fr       */
+/*   Updated: 2023/10/12 19:34:25 by ecarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,5 @@
 # include <stdarg.h>
 
 int	ft_printf(const char *format, ...);
-
-int	ft_putchar(char c);
-int	ft_putstr(char *str);
-int	ft_putptr(void *ptr);
-int	ft_putnbr(int nb);
-int	ft_putunbr(unsigned int nb);
-int	ft_puthex(int nb);
-
 
 #endif

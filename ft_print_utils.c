31e539ecd9ad916acefc108df9e6a2a32e33366a
Err-G <eriklustosa@gmail.com>
2023-10-15 15:33:04 +0100

    minimizing?

--- ft_print_utils.c
+++ ft_print_utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_char.c                                    :+:      :+:    :+:   */
+/*   ft_print_utils.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ecarvalh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ecarvalh <ecarvalh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/12 19:52:39 by ecarvalh          #+#    #+#             */
-/*   Updated: 2023/10/12 20:24:51 by ecarvalh         ###   ########.fr       */
+/*   Created: 2023/10/15 15:31:09 by ecarvalh          #+#    #+#             */
+/*   Updated: 2023/10/15 15:31:32 by ecarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,26 @@ int	ft_print_char(char c)
 {
 	ft_putchar_fd(c, 1);
 	return (1);
+}
+
+int	ft_print_str(char *str)
+{
+	int	len;
+
+	if (!str)
+		return (ft_print_str("(null)"));
+	len = ft_strlen(str);
+	write(1, str, len);
+	return (len);
+}
+
+int	ft_print_ptr(size_t ptr)
+{
+	int	len;
+
+	if (!ptr)
+		return (write(1, "(nil)", 5));
+	len = write(1, "0x", 2);
+	len += ft_print_sbase(ptr, HEX_LOWER);
+	return (len);
 }
